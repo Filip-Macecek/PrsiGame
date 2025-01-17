4447349cfd96a085084f7f1b199bbb792c245335
Filip-Macecek <filip.macecek@mews.com>
2025-01-17 20:12:59 +0100

    change repo structure

---
+++ web/PrsiWeb/Controllers/SessionController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrsiWeb.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SessionController : ControllerBase
+{
+    private readonly ILogger<SessionController> _logger;
+
+    public SessionController(ILogger<SessionController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPut(Name = "GetWeatherForecast")]
+    public async Task Put()
+    {
+
+    }
+}
